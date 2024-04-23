c41e7f0fe075d17ad011af526946bae604c48242
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-04-23 22:06:39 +0000

    chore: install and configure contentlayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { allPosts } from 'contentlayer/generated'
+
 export default function Home () {
+  const posts = allPosts.map((post) => post)
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Teste</h1>
+      <div dangerouslySetInnerHTML={{ __html: posts[0].title }} />
     </main>
   )
 }
